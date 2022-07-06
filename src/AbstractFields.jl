7339573907dbe04bc605cd56ce3549ac5e12c83f
MarcVincent9 <marc.vincent282@gmail.com>
2022-07-06 04:38:45 +0200

    Initial code

--- src/AbstractFields.jl
+++ src/AbstractFields.jl
@@ -1,5 +1,118 @@
 module AbstractFields
 
-# Write your package code here.
+export @abstractfields, @inheritfields
+
+import Base: fieldnames, fieldname, hasfield, fieldcount, getproperty
+
+using MacroTools
+
+
+abstract type AbstractStruct end
+
+_abstractfields_dict = Dict{Symbol, Array{Union{Expr, Symbol}}}()
+
+
+macro abstractfields(ex)
+    @capture(ex, struct T_ fields__ end) || error(
+        "@abstractfields takes a struct declaration as argument")
+    if @capture(T, B_ <: A_)
+        _abstractfields_dict[B] = [_abstractfields_dict[A]; fields]
+        res = quote
+            abstract type $B <: $A end
+        end
+        esc(res) # evaluate in the macro call environment to find parent type
+    else
+        _abstractfields_dict[T] = fields
+        quote
+            abstract type $T <: AbstractStruct end
+        end
+    end
+end
+
+macro inheritfields(ex)
+    @capture(ex, struct B_ <: A_ fields__ end) || error(
+        "@inheritfields takes a struct subtype declaration as argument")
+    A in keys(_abstractfields_dict) || error(
+        "invalid subtyping in definition of $T: 
+        parent type $A must be declared with @abstractfields")
+    res = quote
+        struct $B <: $A
+            $(_abstractfields_dict[A]...)
+            $(fields...)
+        end
+    end
+    esc(res) # evaluate in the macro call environment to find parent type
+end
+
+
+macro invoke(ex)
+    @capture(ex, f_(args__)) || error(
+        "@invoke takes a function call as argument")
+    types = map(x -> @capture(x, _::T_) ? T : :Any, args)
+    res = quote
+        invoke($f, Tuple{$(types...)}, $(args...))
+    end
+    esc(res)
+end
+
+function getfields(T::Type{<:AbstractStruct})
+    _abstractfields_dict[nameof(T)]
+end
+
+function getname(field::Union{Expr, Symbol})
+    (field isa Expr ? field.args[1] : field)::Symbol
+end
+
+function gettype(field::Union{Expr, Symbol})
+    (field isa Expr ? eval(field.args[2]) : Any)::Type
+end
+
+
+function fieldnames(T::Type{<:AbstractStruct})
+    if isabstracttype(T)
+        Tuple(map(getname, getfields(T)))
+    else
+        @invoke fieldnames(T::DataType)
+    end
+end
+
+function fieldname(T::Type{<:AbstractStruct}, i::Integer)
+    if isabstracttype(T)
+        getname(getfields(T)[i])::Symbol
+    else
+        @invoke fieldname(T::DataType, i::Integer)
+    end
+end
+
+function hasfield(T::Type{<:AbstractStruct}, name::Symbol)
+    if isabstracttype(T)
+        name in map(getname, getfields(T))
+    else
+        @invoke hasfield(T::DataType, name::Symbol)
+    end
+end
+
+function fieldcount(T::Type{<:AbstractStruct})
+    if isabstracttype(T)
+        length(getfields(T))
+    else
+        @invoke fieldcount(T::DataType)
+    end
+end
+
+function getproperty(value::Type{<:AbstractStruct}, name::Symbol)
+    if name === :types && isabstracttype(value)
+        Core.svec(map(gettype, getfields(value))...)
+    else
+        @invoke getproperty(value::DataType, name::Symbol)
+    end
+end
 
 end
+
+# TODO parametric types? if not possible:
+# macro abstractfields(ex)
+#     @capture(ex, struct T_Symbol fields__ end)
+# ...
+# should be possible with postwalk!
+
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,44 @@
 using AbstractFields
 using Test
 
-@testset "AbstractFields.jl" begin
-    # Write your tests here.
+macro testall(T, fields, types)
+name = "$T"
+quote
+    @testset $name begin
+        @test fieldnames($T) == $fields
+        for (index, field) in enumerate($fields)
+            @test fieldname($T, index) === field
+            @test hasfield($T, field)
+        end
+        @test fieldcount($T) == length($fields)
+        @test $T.types == $types
+    end
 end
+end
+
+@testset "AbstractFields.jl" begin
+    @abstractfields struct Foo
+        x::Int
+        y
+    end
+
+    @abstractfields struct Bar <: Foo
+        w::String
+    end
+
+    @inheritfields struct FooBar <: Bar
+        z
+    end
+
+    struct BarFoo <: Foo
+        x::Int
+        # drop y
+        a
+    end
+    
+    @testall(Foo, (:x, :y), Core.svec(Int64, Any))
+    @testall(Bar, (:x, :y, :w), Core.svec(Int64, Any, String))
+    @testall(FooBar, (:x, :y, :w, :z), Core.svec(Int64, Any, String, Any))
+    @testall(BarFoo, (:x, :a), Core.svec(Int64, Any))
+end
+
